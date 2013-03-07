0a5a8604d81719272fa8d4f22b917a3727741aae
dewey <mail@notmyhostna.me>
2013-03-07 15:50:12 +0100

    And another one.
    Conflicts:
    	plugin.php

---
+++ README.md
@@ -0,0 +1,13 @@
+# Piwik Analytics Plugin for Koken CMS
+===
+
+Add Piwik Analytics to your [Koken](http://koken.me) CMS.
+
+## Instructions:
+
+- Navigate to your Koken Plugin Directory `storage/plugins/`
+- Run `git clone https://github.com/dewey/koken-piwik-analytics.git`
+- Make sure the permissions are correct. `chown -R www-data:www-data koken-piwik-analytics`
+- The Plugin should now show up in your AdminCP/Plugins/ section.
+- Enable the Plugin, click on Setup and add the correct path to your Piwik installation. **Important:** No trailing slashes!
+- Done!
--- plugin.php
+++ plugin.php
@@ -14,15 +14,15 @@ class KokenPiwikAnalytics extends KokenPlugin {
 		echo <<<OUT
 <!-- Piwik --> 
 <script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "$this->data->tracking_url" : "$this->data->tracking_url");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+var pkBaseURL = (("https:" == document.location.protocol) ? "{$this->data->tracking_url_ssl}" : "{$this->data->tracking_url}");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "/piwik.js' type='text/javascript'%3E%3C/script%3E"));
 </script><script type="text/javascript">
 try {
 var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 3);
 piwikTracker.trackPageView();
 piwikTracker.enableLinkTracking();
 } catch( err ) {}
-</script><noscript><p><img src="$this->data->tracking_url/piwik.php?idsite=$this->data->site_id" style="border:0" alt="" /></p></noscript>
+</script><noscript><p><img src="{$this->data->tracking_url}/piwik.php?idsite={$this->data->site_id}" style="border:0" alt="" /></p></noscript>
 <!-- End Piwik Tracking Code -->
 OUT;
 
